94d7c8f4746f7519cf9a4afb04dc2f438de8e7de
yannickferenczi <>
2023-08-24 20:02:25 +0000

    Update the settings and home  apps

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+# Create your views here.
+
+
 def index(request):
-    """This function render the landing page of our web application"""
+    """ A view to return the index page """
+
     return render(request, 'home/index.html')
