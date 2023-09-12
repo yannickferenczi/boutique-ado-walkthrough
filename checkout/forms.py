4dca04ace688a8df0a383cb165388ff8aa99c28a
yannickferenczi <>
2023-09-12 08:04:36 +0000

    Add checkout views and template

--- checkout/forms.py
+++ checkout/forms.py
@@ -38,10 +38,10 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields['field'].required:
+            if self.fields[field].required:
                 placeholder = f'{placeholders[field]}*'
             else:
-                placeholder = placeholders[field]'
+                placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
